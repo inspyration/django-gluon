7be98641a916dbbe1b74078c3e44abcf847595c3
Sébastien CHAZALLET <s.chazallet@gmail.com>
2014-12-16 11:28:47 +0100

    prepare urls

--- gluon/gluon/urls.py
+++ gluon/gluon/urls.py
@@ -4,4 +4,7 @@ from django.contrib import admin
 urlpatterns = patterns(
     "",
     url(r"^admin/", include(admin.site.urls)),
+#    url(r"^util/", include("util.urls")),
+#    url(r"^saas/", include("saas.urls")),
+#    url(r"^legal/", include("legal.urls")),
 )
