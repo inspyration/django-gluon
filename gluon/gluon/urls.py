99ca6cbdd8509c483f2e4a215278021ccd69457d
Sébastien CHAZALLET <s.chazallet@gmail.com>
2015-01-23 16:07:48 +0100

    Serve media files when in debug mode

--- gluon/gluon/urls.py
+++ gluon/gluon/urls.py
@@ -1,5 +1,9 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
+
 from django.contrib import admin
+
 
 urlpatterns = patterns(
     "",
@@ -8,3 +12,6 @@ urlpatterns = patterns(
     url(r"", include("saas.urls")),
 #    url(r"^legal/", include("legal.urls")),
 )
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
