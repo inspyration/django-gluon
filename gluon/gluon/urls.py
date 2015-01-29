13fa82809bbb6397ea99b88f9d725e9154cea323
Sébastien CHAZALLET <s.chazallet@gmail.com>
2015-01-29 15:21:55 +0100

    Import modules URI automatically

--- gluon/gluon/urls.py
+++ gluon/gluon/urls.py
@@ -4,6 +4,8 @@ from django.conf.urls.static import static
 
 from django.contrib import admin
 
+from pathlib import Path
+
 
 urlpatterns = patterns(
     "",
@@ -12,6 +14,23 @@ urlpatterns = patterns(
     url(r"", include("saas.urls")),
 #    url(r"^legal/", include("legal.urls")),
 )
+
+modules = set()
+if Path("apps.txt").is_file():
+    with open("apps.txt") as f:
+        for line in f.readlines():
+            line = line.strip()
+            if not line or line[0] == "#":
+                continue
+            else:
+                modules.add(line)
+
+if modules:
+    urlpatterns += patterns(
+        "",
+        *(url(r"^modules/{0}/".format(module),
+              include("{0}.urls".format(module), module, module))
+        for module in modules))
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
